bc60a62e039300ac0eaedf15f76fa82309025442
Aman Kaur <amankaur2628@gmail.com>
2020-07-15 14:09:10 -0400

    Recycler view added to the main activity

--- app/src/main/java/com/lambton/contact_amanpreet_c0782918_android/activity/MainActivity.java
+++ app/src/main/java/com/lambton/contact_amanpreet_c0782918_android/activity/MainActivity.java
@@ -1,14 +1,24 @@
-package com.lambton.contact_amanpreet_c0782918_android;
+package com.lambton.contact_amanpreet_c0782918_android.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.lambton.contact_amanpreet_c0782918_android.R;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvPersonList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvPersonList = findViewById(R.id.rvPersonList);
     }
 }
